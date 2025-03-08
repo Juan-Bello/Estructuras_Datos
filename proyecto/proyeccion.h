90b1ac002c1d19b4a1c7787029b938884adf9cd9
Juan Bello <141571725+Juan-Bello@users.noreply.github.com>
2025-03-07 20:55:21 -0500

    Add files via upload

--- proyecto/proyeccion.h
+++ proyecto/proyeccion.h
@@ -1,9 +1,16 @@
+/**********************************
+Fecha 5 de Marzo 2025
+Autor: Santiago Hernandez, Juan Esteban, Esteban Navas
+Materia: Estructuras de Datos
+Tema: Primera entrega Proyecto
+***********************************/
 #ifndef PROYECCION_H
 #define PROYECCION_H
 
-#include "imagen.h"
-#include <algorithm>
-#include <numeric>
+#include "imagen.h" // Incluir la definición de la estructura imagen y volumen
+#include <algorithm> // Incluir la librería para usar funciones como min_element, max_element, etc.
+
+// Declaración de la función para generar una proyección 2D
 imagen proyeccion2D(volumen vol, string direccion, string criterio);
 
 #endif // PROYECCION_H
