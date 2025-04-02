fabd489ff75ad4a55957cdf48c2a95f723af7c97
Juan Bello <141571725+Juan-Bello@users.noreply.github.com>
2025-04-02 12:09:10 -0500

    correcion valores red-black_tree.cpp

--- red-black_tree/red-black_tree.cpp
+++ red-black_tree/red-black_tree.cpp
@@ -2,6 +2,12 @@
 http://quiz.geeksforgeeks.org/c-program-red-black-tree-insertion/
    This code is adopted from the code provided by
    Dinesh Khandelwal in comments **/
+/********************************
+* Autor:Juan Esteban Bello 
+* Materia: Estructuras de Datos                 
+* Tema: Red Black Tree
+*********************************/
+
 #include <bits/stdc++.h>
 #include "redblacktree.h"
 using namespace std;
@@ -10,14 +16,16 @@ int main()
 {
     RBTree tree;
  
+    tree.insert(11);
+    tree.insert(19);
     tree.insert(7);
-    tree.insert(6);
-    tree.insert(5);
-    tree.insert(4);
-    tree.insert(3);
-    tree.insert(2);
-    tree.insert(1);
- 
+    tree.insert(14);
+    tree.insert(17);
+    tree.insert(31);
+    tree.insert(26);
+    tree.insert(45);
+    tree.insert(70);
+   
     cout << "Inoder Traversal of Created Tree\n";
     tree.inorder();
  
@@ -27,4 +35,5 @@ int main()
  
     return 0;
 }
- 
+// Conclusión: Al insertar los valores de las diapositivas vistas en clase (las cuales presentan el ejercicio incompleto) se observa que el resultado in order final es : 7 11 14 17 19 26 31 45 70
+// y Level Order es 17 11 26 7 14 19 45 31 70
